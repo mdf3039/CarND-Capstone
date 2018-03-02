5e9421e524aa5d94bff188f111759f6059b420b7
mdf3039 <mdf3039@gmail.com>
2018-03-02 08:31:31 -0600

    93rd - getting the parameters working for the two PID controllers - angle first

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -126,7 +126,7 @@ class DBWNode(object):
             rospy.loginfo("The CTE: " + str(self.cte))
             kp_cte = 0.2
             ki_cte = 0.0#.08 # 1.015
-            kd_cte = 2.0#.35 # 0.5
+            kd_cte = 1.0#.35 # 0.5
             pid_step_cte = max(min(self.pid_controller_cte.step(self.cte, self.sample_time, kp_cte, ki_cte, kd_cte), 8), -8)
             # The difference in the angle will also affect the steering angle
             # Since the angle is not accurate, use the previous position
