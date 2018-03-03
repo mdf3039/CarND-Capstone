ed81dcfcf79e5f4d455e2d641a3e4a36dec7aade
mdf3039 <mdf3039@gmail.com>
2018-03-03 10:10:14 -0600

    94th - predicting the curvature of the road into the future

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -126,15 +126,20 @@ class DBWNode(object):
             wp_distances = ((self.base_waypoints-msg)**2).sum(axis=1)
             #find and append the closest, fourth, and eighth point
             circle_points = self.base_waypoints[np.argmin(wp_distances)]
+            rospy.loginfo("circle_points: " + circle_points.shape)
             circle_points = np.vstack((circle_points, self.base_waypoints[(np.argmin(wp_distances)+4)%len(wp_distances)]))
+            rospy.loginfo("circle_points: " + circle_points.shape)
             circle_points = np.vstack((circle_points, self.base_waypoints[(np.argmin(wp_distances)+8)%len(wp_distances)]))
+            rospy.loginfo("circle_points: " + circle_points.shape)
             #use the three points to find the radius of the circle
             eval_matrix = np.hstack((-2*circle_points[:,0],-2*circle_points[:,1],(circle_points**2).sum(axis=1)))
+            rospy.loginfo("eval_matrix: " + eval_matrix.shape)
             #subtract the last entry of the eval matrix from the others and keep the first two rows
             eval_matrix = np.subtract(eval_matrix,eval_matrix[2])[0:2]
             try:
                 x = np.linalg.solve(eval_matrix[:,0:2],eval_matrix[:,2])
                 radius = (((msg-x)**2).sum()*1.0)**(1.0/2)
+                rospy.loginfo("Radius: " + radius)
                 #convert the angle into degrees then divide by the steering ratio to get the steer value
                 angle = np.arcsin(self.wheel_base/radius) * (180.0/np.pi)
                 steer_value = angle * self.steer_ratio
@@ -161,7 +166,7 @@ class DBWNode(object):
                 self.cte *= -1
             rospy.loginfo("The CTE: " + str(self.cte))
             kp_cte = 0.5###07 best is 0.31, .41
-            ki_cte = 0.0#16#.08 # 1.015
+            ki_cte = 2.0#16#.08 # 1.015
             kd_cte = 0.3#.35 # 0.5
             pid_step_cte = max(min(self.pid_controller_cte.step(self.cte, self.sample_time, kp_cte, ki_cte, kd_cte), 8), -8)
             # The difference in the angle will also affect the steering angle
